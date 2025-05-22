60a1b6561a0b4ae62d6ccc49535794b67953fa7c
Alex Augusto <alexaugustoti@gmail.com>
2025-05-22 14:15:58 -0300

    EMS - 02.07.06 - Dependencies

--- src/main/java/com/algaworks/algashop/ordering/domain/model/valueobject/id/OrderItemId.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/valueobject/id/OrderItemId.java
@@ -1,30 +1,21 @@
 package com.algaworks.algashop.ordering.domain.model.valueobject.id;
 
 import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
-import io.hypersistence.tsid.TSID;
 
 import java.util.Objects;
 
-public record OrderItemId(TSID value) {
+public record OrderItemId(String value) {
 
     public OrderItemId {
         Objects.requireNonNull(value);
     }
 
     public OrderItemId() {
-        this(IdGenerator.gererateTSID());
-    }
-
-    public OrderItemId(Long value) {
-        this(TSID.from(value));
-    }
-
-    public OrderItemId(String value) {
-        this(TSID.from(value));
+        this(IdGenerator.gererateTSID().toString());
     }
 
     @Override
     public String toString() {
-        return value.toString();
+        return value;
     }
 }
