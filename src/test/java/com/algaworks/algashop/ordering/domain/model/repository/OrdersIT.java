08418c2024261d219d20b84bc54a686064987453
Alex Augusto <alexaugustoti@gmail.com>
2025-05-30 16:48:14 -0300

    EMS - 02.08.17

--- src/test/java/com/algaworks/algashop/ordering/domain/model/repository/OrdersIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/repository/OrdersIT.java
@@ -71,4 +71,25 @@ class OrdersIT {
         Assertions.assertThat(order.isPaid()).isTrue();
 
     }
-}
+
+    @Test
+    public void shouldNotAllowStaleUpdates() {
+        Order order = OrderTestDataBuilder.anOrder().status(OrderStatus.PLACED).build();
+        orders.add(order);
+
+        Order orderT1 = orders.ofId(order.id()).orElseThrow();
+        Order orderT2 = orders.ofId(order.id()).orElseThrow();
+
+        orderT1.markAsPaid();
+        orders.add(orderT1);
+
+        orderT2.cancel();
+        orders.add(orderT2);
+
+        Order savedOrder = orders.ofId(order.id()).orElseThrow();
+
+        Assertions.assertThat(savedOrder.paidAt()).isNotNull();
+        Assertions.assertThat(savedOrder.canceledAt()).isNotNull();
+    }
+
+ }
