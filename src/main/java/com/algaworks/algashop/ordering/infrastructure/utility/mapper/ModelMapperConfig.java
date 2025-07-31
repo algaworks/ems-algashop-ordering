1478d0d1fb86ede193b510c69b4b9dc654a1f77a
Alex Silva <alexaugustoti@gmail.com>
2025-07-31 08:35:58 -0300

    EMS 02.12.05

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/utility/mapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/utility/mapper/ModelMapperConfig.java
@@ -2,6 +2,8 @@ package com.algaworks.algashop.ordering.infrastructure.utility.mapper;
 
 import com.algaworks.algashop.ordering.application.utility.Mapper;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.convention.NamingConventions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,10 @@ public class ModelMapperConfig {
     @Bean
     public Mapper mapper() {
         ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSourceNamingConvention(NamingConventions.NONE)
+                .setDestinationNamingConvention(NamingConventions.NONE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
         return new Mapper() {
             @Override
             public <D> D convert(Object source, Class<D> destinationType) {
