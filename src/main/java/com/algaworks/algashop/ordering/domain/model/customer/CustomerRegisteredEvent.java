78cec6f05139b22e3b246634f32bfafe19d493f3
Alex Silva <alexaugustoti@gmail.com>
2025-08-04 16:09:21 -0300

    EMS 02.13.04

--- src/main/java/com/algaworks/algashop/ordering/domain/model/customer/CustomerRegisteredEvent.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/customer/CustomerRegisteredEvent.java
@@ -1,7 +1,6 @@
 package com.algaworks.algashop.ordering.domain.model.customer;
 
 import java.time.OffsetDateTime;
-import java.time.OffsetTime;
 
 public record CustomerRegisteredEvent(CustomerId customerId, OffsetDateTime registeredAt) {
 }
