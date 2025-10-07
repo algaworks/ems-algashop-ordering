6ebaf3cc77e5f6f1261215106d4c5779573cd08e
Alex Silva <alexaugustoti@gmail.com>
2025-10-07 09:56:13 -0300

    EMS - 03.06.17 - Unique key

--- src/test/java/com/algaworks/algashop/ordering/presentation/shoppincart/ShoppingCartControllerIT.java
+++ src/test/java/com/algaworks/algashop/ordering/presentation/shoppincart/ShoppingCartControllerIT.java
@@ -45,6 +45,7 @@ public class ShoppingCartControllerIT {
     private ShoppingCartPersistenceEntityRepository shoppingCartRepository;
 
     private static final UUID validCustomerId = UUID.fromString("6e148bd5-47f6-4022-b9da-07cfaa294f7a");
+    private static final UUID validShoppingCartId = UUID.fromString("4f31582a-66e6-4601-a9d3-ff608c2d4461");
 
     private WireMockServer wireMockProductCatalog;
     private WireMockServer wireMockRapidex;
@@ -104,14 +105,6 @@ public class ShoppingCartControllerIT {
 
     @Test
     public void shouldAddProductToShoppingCart() {
-        var shoppingCartPersistence = existingShoppingCart().items(new HashSet<>())
-                .customer(customerRepository.getReferenceById(validCustomerId))
-                .build();
-
-        shoppingCartRepository.save(shoppingCartPersistence);
-
-        UUID shoppingCartId = shoppingCartPersistence.getId();
-
         String json = AlgaShopResourceUtils.readContent("json/add-product-to-shopping-cart.json");
 
         RestAssured
@@ -120,13 +113,13 @@ public class ShoppingCartControllerIT {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(json)
             .when()
-                .post("/api/v1/shopping-carts/{shoppingCartId}/items", shoppingCartId)
+                .post("/api/v1/shopping-carts/{shoppingCartId}/items", validShoppingCartId)
             .then()
                 .assertThat()
                 .statusCode(HttpStatus.NO_CONTENT.value());
 
-        var shoppingCartPersistenceEntity = shoppingCartRepository.findById(shoppingCartPersistence.getId()).orElseThrow();
-        Assertions.assertThat(shoppingCartPersistenceEntity.getTotalItems()).isEqualTo(2);
+        var shoppingCartPersistenceEntity = shoppingCartRepository.findById(validShoppingCartId).orElseThrow();
+        Assertions.assertThat(shoppingCartPersistenceEntity.getTotalItems()).isEqualTo(4);
     }
 
 }
