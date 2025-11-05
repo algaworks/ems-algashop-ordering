311b55c6004bd2a944359ef4ee974ac25d85a135
Alex Silva <alexaugustoti@gmail.com>
2025-11-05 16:15:01 -0300

    EMS - 03.11.09

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/AbstractPersistenceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/AbstractPersistenceIT.java
@@ -1,11 +1,10 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence;
 
+import com.algaworks.algashop.ordering.infrastructure.config.auditing.SpringDataAuditingConfig;
 import com.algaworks.algashop.ordering.utils.TestcontainerPostgreSQLConfig;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
