f87861fd743adaa9ae6e73b4e0148a4c78fc2fab
Alex Silva <alexaugustoti@gmail.com>
2025-11-05 14:14:27 -0300

    EMS - 03.11.06

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/persistence/shoppingcart/ForObtainingShoppingCartsJpaRepositoryImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/persistence/shoppingcart/ForObtainingShoppingCartsJpaRepositoryImpl.java
@@ -1,19 +1,19 @@
-package com.algaworks.algashop.ordering.infrastructure.persistence.shoppingcart;
+package com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.shoppingcart;
 
 import com.algaworks.algashop.ordering.core.application.utility.Mapper;
 import com.algaworks.algashop.ordering.core.domain.model.shoppingcart.ShoppingCartNotFoundException;
 import com.algaworks.algashop.ordering.core.ports.in.shoppingcart.ShoppingCartOutput;
 import com.algaworks.algashop.ordering.core.ports.out.shoppingcart.ForObtainingShoppingCarts;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-@Service
+@Component
 @RequiredArgsConstructor
 @Transactional
-public class ShoppingCartQueryServiceImpl implements ForObtainingShoppingCarts {
+public class ForObtainingShoppingCartsJpaRepositoryImpl implements ForObtainingShoppingCarts {
 
     private final ShoppingCartPersistenceEntityRepository persistenceRepository;
     private final Mapper mapper;
