e73e1fd67df634188255a0f9916fba09eda517d8
Alex Silva <alexaugustoti@gmail.com>
2025-11-05 15:14:46 -0300

    EMS - 03.11.07

--- src/test/java/com/algaworks/algashop/ordering/core/application/customer/loyaltypoints/CustomerLoyaltyPointsApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/core/application/customer/loyaltypoints/CustomerLoyaltyPointsApplicationServiceIT.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.core.application.customer.loyaltypoints;
 
 import com.algaworks.algashop.ordering.core.application.AbstractApplicationIT;
+import com.algaworks.algashop.ordering.core.application.customer.CustomerLoyaltyPointsApplicationService;
 import com.algaworks.algashop.ordering.core.domain.model.commons.Email;
 import com.algaworks.algashop.ordering.core.domain.model.commons.Money;
 import com.algaworks.algashop.ordering.core.domain.model.commons.Quantity;
@@ -8,7 +9,7 @@ import com.algaworks.algashop.ordering.core.domain.model.customer.*;
 import com.algaworks.algashop.ordering.core.domain.model.order.*;
 import com.algaworks.algashop.ordering.core.domain.model.product.Product;
 import com.algaworks.algashop.ordering.core.domain.model.product.ProductTestDataBuilder;
-import com.algaworks.algashop.ordering.infrastructure.listener.customer.CustomerEventListener;
+import com.algaworks.algashop.ordering.infrastructure.adapters.in.listener.customer.CustomerEventListener;
 import io.hypersistence.tsid.TSID;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
