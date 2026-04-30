eae08e1860cc4cbbfbeca4ee35f891118b788b80
Alex Silva <alex.silva@algaworks.com>
2026-04-30 13:20:49 -0300

    feat: EMS - 05.10.07 - Limitando consulta de pedidos

--- src/main/java/com/algaworks/algashop/ordering/core/application/security/SecurityCheckApplicationService.java
+++ src/main/java/com/algaworks/algashop/ordering/core/application/security/SecurityCheckApplicationService.java
@@ -6,5 +6,6 @@ public interface SecurityCheckApplicationService {
     UUID getAuthenticatedUserId();
     boolean isAuthenticated();
     boolean isMachineAuthenticated();
+    boolean isCustomer();
 }
 
