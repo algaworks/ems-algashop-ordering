7af513970a57d47e888e3bd7a75b84989fffeefc
Alex Silva <alex.silva@algaworks.com>
2026-05-06 09:13:29 -0300

    EMS - 05.12.01 - Avaliando testes

--- src/test/java/com/algaworks/algashop/ordering/core/application/checkout/BuyNowApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/core/application/checkout/BuyNowApplicationServiceIT.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDate;
