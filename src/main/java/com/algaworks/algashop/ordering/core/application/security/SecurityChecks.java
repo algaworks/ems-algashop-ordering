35ff9f904cb694c7ce06e081b2665335e0cdce0d
Alex Silva <alex.silva@algaworks.com>
2026-05-07 10:20:04 -0300

    EMS - 01.12.03 - Garantindo regras de segurança

--- src/main/java/com/algaworks/algashop/ordering/core/application/security/SecurityChecks.java
+++ src/main/java/com/algaworks/algashop/ordering/core/application/security/SecurityChecks.java
@@ -2,10 +2,11 @@ package com.algaworks.algashop.ordering.core.application.security;
 
 import java.util.UUID;
 
-public interface SecurityCheckApplicationService {
+public interface SecurityChecks {
     UUID getAuthenticatedUserId();
     boolean isAuthenticated();
     boolean isMachineAuthenticated();
     boolean isCustomer();
+    boolean canOrderFor(UUID customerId);
 }
 
--- src/test/java/com/algaworks/algashop/ordering/core/application/AbstractApplicationIT.java
+++ src/test/java/com/algaworks/algashop/ordering/core/application/AbstractApplicationIT.java
@@ -1,17 +1,11 @@
 package com.algaworks.algashop.ordering.core.application;
 
-import com.algaworks.algashop.ordering.core.application.security.SecurityCheckApplicationService;
-import com.algaworks.algashop.ordering.core.domain.model.customer.CustomerTestDataBuilder;
 import com.algaworks.algashop.ordering.utils.MockJwtDecoderConfig;
 import com.algaworks.algashop.ordering.utils.TestcontainerPostgreSQLConfig;
 import com.algaworks.algashop.ordering.utils.WithMockJwt;
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
