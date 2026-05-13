2d914d13945f0d52f1e68d7bbbdf886522772e80
Alex Silva <alex.silva@algaworks.com>
2026-05-12 21:04:10 -0300

    EMS - 05.14.03 - Implementando recursos 'me' de Customer

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/config/security/SecurityAnnotations.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/config/security/SecurityAnnotations.java
@@ -21,13 +21,8 @@ public class SecurityAnnotations {
 
     @Target({ElementType.METHOD, ElementType.TYPE})
     @Retention(RetentionPolicy.RUNTIME)
-    @PreAuthorize("hasAuthority('SCOPE_customers:read')")
+    @PreAuthorize("hasAuthority('SCOPE_customers:read') and not hasRole('CUSTOMER')")
     public @interface CanReadCustomers {}
-
-    @Target({ElementType.METHOD, ElementType.TYPE})
-    @Retention(RetentionPolicy.RUNTIME)
-    @PreAuthorize("hasAuthority('SCOPE_customers:write')")
-    public @interface CanWriteCustomers {}
 
     @Target({ElementType.METHOD, ElementType.TYPE})
     @Retention(RetentionPolicy.RUNTIME)
@@ -39,5 +34,16 @@ public class SecurityAnnotations {
     @PreAuthorize("hasAuthority('SCOPE_shopping-carts:write')")
     public @interface CanWriteShoppingCarts {}
 
+    @Target({ElementType.METHOD, ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @PreAuthorize("hasAuthority('SCOPE_customers:write') and hasRole('CUSTOMER')")
+	public @interface CanWriteMyCustomerProfile {
+	}
+
+    @Target({ElementType.METHOD, ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @PreAuthorize("hasAuthority('SCOPE_customers:read') and hasRole('CUSTOMER')")
+    public @interface CanReadMyCustomerProfile {
+    }
 }
 
