bf23411b1dbb37c29ebda2f5aa6d37d6311952a3
Alex Silva <alex.silva@algaworks.com>
2026-08-26 10:15:54 -0300

    fix: Order Disassembler não copiava shipping e billing

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/persistence/order/OrderPersistenceEntityTestDataBuilder.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/persistence/order/OrderPersistenceEntityTestDataBuilder.java
@@ -1,10 +1,12 @@
 package com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.order;
 
 import com.algaworks.algashop.ordering.core.domain.model.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.commons.AddressEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.customer.CustomerPersistenceEntityTestDataBuilder;
 import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.order.OrderPersistenceEntity.OrderPersistenceEntityBuilder;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.Set;
 
@@ -22,6 +24,41 @@ public class OrderPersistenceEntityTestDataBuilder {
                 .status("DRAFT")
                 .paymentMethod("CREDIT_CARD")
                 .placedAt(OffsetDateTime.now())
+                .billing(BillingEmbeddable.builder()
+                        .firstName("John")
+                        .lastName("Doe")
+                        .document("25508578")
+                        .phone("478-256-2604")
+                        .email("johndoe@email.com")
+                        .address(AddressEmbeddable.builder()
+                                .street("Bourbon Street")
+                                .number("1134")
+                                .complement("Apt. 114")
+                                .neighborhood("New Orleans")
+                                .city("Louisiana")
+                                .state("Louisiana")
+                                .zipCode("70130")
+                                .build())
+                        .build())
+                .shipping(ShippingEmbeddable.builder()
+                        .cost(new BigDecimal("25.00"))
+                        .expectedDate(LocalDate.of(2025, 9, 30))
+                        .recipient(RecipientEmbeddable.builder()
+                                .firstName("John")
+                                .lastName("Doe")
+                                .document("25508578")
+                                .phone("478-256-2604")
+                                .build())
+                        .address(AddressEmbeddable.builder()
+                                .street("Bourbon Street")
+                                .number("1134")
+                                .complement("Apt. 114")
+                                .neighborhood("North Ville")
+                                .city("North Ville")
+                                .state("New Orleans")
+                                .zipCode("70130")
+                                .build())
+                        .build())
                 .items(Set.of(
                         existingItem().build(),
                         existingItemAlt().build()
