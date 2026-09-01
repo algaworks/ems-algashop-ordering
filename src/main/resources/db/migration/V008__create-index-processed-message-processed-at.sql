916b72633996ba7345b35217d8e6eadee6f2e0e3
Alex Silva <alex.silva@algaworks.com>
2026-09-01 09:55:38 -0300

    fix: Criação do indice

--- src/main/resources/db/migration/V008__create-index-processed-message-processed-at.sql
+++ src/main/resources/db/migration/V008__create-index-processed-message-processed-at.sql
@@ -1,2 +1,2 @@
-create index concurrently if not exists ix_processed_message_processed_at
+create index if not exists ix_processed_message_processed_at
     on public.processed_message (processed_at);
